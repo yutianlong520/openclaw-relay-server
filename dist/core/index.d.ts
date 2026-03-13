34abccd5c87f5e3624f1da05987b3c48dd6b4625
Sky <83815270+yutianlong520@users.noreply.github.com>
2026-03-13 21:18:11 +0800

    Add dist/core/index.d.ts

---
+++ dist/core/index.d.ts
@@ -0,0 +1,4 @@
+export { default as connectionManager } from './ConnectionManager';
+export { default as messageRouter } from './MessageRouter';
+export { default as webSocketServer } from './WebSocketServer';
+//# sourceMappingURL=index.d.ts.map
