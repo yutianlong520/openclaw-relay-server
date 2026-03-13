5281c1ccb6b8aa58f898774153903699e654c909
Sky <83815270+yutianlong520@users.noreply.github.com>
2026-03-13 21:19:47 +0800

    Add dist/utils/validator.d.ts

---
+++ dist/utils/validator.d.ts
@@ -0,0 +1,12 @@
+export declare function isValidEmail(email: string): boolean;
+export declare function isValidUsername(username: string): boolean;
+export declare function isValidPassword(password: string): boolean;
+export declare function isValidDeviceId(deviceId: string): boolean;
+export declare function isValidMessageType(type: string): boolean;
+export declare function isValidClientType(type: string): boolean;
+export declare function sanitizeInput(input: string, maxLength?: number): string;
+export declare function validatePagination(page?: any, pageSize?: any): {
+    page: number;
+    pageSize: number;
+};
+//# sourceMappingURL=validator.d.ts.map
