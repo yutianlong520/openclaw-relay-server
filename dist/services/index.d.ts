5bd56508f52c7a581a51afa350c8e74d3a491190
Sky <83815270+yutianlong520@users.noreply.github.com>
2026-03-13 21:19:00 +0800

    Add dist/services/index.d.ts

---
+++ dist/services/index.d.ts
@@ -0,0 +1,5 @@
+export { default as authService } from './AuthService';
+export { default as userService } from './UserService';
+export { default as messageService } from './MessageService';
+export { default as deviceService } from './DeviceService';
+//# sourceMappingURL=index.d.ts.map
